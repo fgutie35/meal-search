c75b4013c51ec08f6acc6db2e6f3d7c1af6e1708
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-12-04 07:35:05 -0600

    Fixed navigation

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -3,7 +3,7 @@
     <RouterLink :to="{name: 'Home'}">Home</RouterLink>
     <RouterLink :to="{name: 'MealsByName'}">MealsByName</RouterLink>
     <RouterLink :to="{name: 'MealsByLetter'}">MealsByLetter</RouterLink>
-    <RouterLink :to="{name: 'MealsByIngredients'}">MealsByIngredients</RouterLink>
+    <RouterLink :to="{name: 'Ingredients'}">Meals By Ingredients</RouterLink>
 
   </nav>
 </template>
